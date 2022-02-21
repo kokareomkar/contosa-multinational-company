26f0a27fa52d0c89117aa2401b0b9c3de3b2b518
Omkar Kokare <okokareoo72@gmail.com>
2022-02-21 18:34:50 +0530

    Exam

---
+++ server (1).js
@@ -0,0 +1,16 @@
+var express =require("express");  //lib-----Node Module
+var app=express();  // will create app object from express package
+
+var onAboutUs=function(req, res){
+    res.send("Chief Mentor :Ravi Tambade");
+};
+var onDefault=function(req, res){
+    res.send("<h1> contoso multinational company Pvt. Ltd</h1>"+
+              "<hr/>"+
+              "<ol>"
+             "</ol>");
+};
+app.get("/",onDefault);  // Request handler functions are registered
+app.get("/aboutus",onAboutUs);  // Request handler functions are registered
+var server=app.listen(8081);
+console.log("Server is running on port 8081");
